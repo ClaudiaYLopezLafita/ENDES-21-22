b5cf9c2513bd2811468d20c6bbf713a81ab6ad25
ClaudiaYLopezLafita <alum.clopezl@iesalixar.org>
2021-12-11 18:24:49 +0100

    Comentarios añadidos al código

--- Prueba_git_GH/HolaMundo.java
+++ Prueba_git_GH/HolaMundo.java
@@ -1,5 +1,12 @@
+/*
+*Muestra por pantalla la frase "Hola Mundo"
+*
+* @author Clodo
+*/
+
+
 public class HolaMundo{
   public static void main{
-    System.out.println("Hola Mundo")
+    System.out.println("Hola Mundo");
   }
 }
