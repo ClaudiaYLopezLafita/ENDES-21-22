5827d808e7b364570593a02387a819c459c8d741
ClaudiaYLopezLafita <alum.clopezl@iesalixar.org>
2021-12-11 20:48:32 +0100

    Colores definido

--- Prueba_git_GH/HolaMundo.java
+++ Prueba_git_GH/HolaMundo.java
@@ -6,7 +6,10 @@
 
 
 public class HolaMundo{
-  public static void main{
+  public static void main(String[] args){
+    String naranja = "\033[33m";
+    String azul = "\033[34m";
+  
     System.out.println("Hola Mundo");
     System.out.println("Aquí estamos haciendo pruebas de Git.");
   }
